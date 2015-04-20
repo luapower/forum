cd1ff1f4595e0f1aa2e71e3285e0ce28e198f9e8
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2015-04-20 03:38:55 +0000

    daily backup

--- .mgit/backup.sh
+++ .mgit/backup.sh
@@ -5,7 +5,6 @@
 # backup redis
 rm -f .mgit/dump.rdb.gz
 sudo gzip /var/lib/redis/dump.rdb -c | openssl enc -aes-256-cbc -k "$(cat .mgit/secret)" -out .mgit/dump.rdb.gz || exit 1
-exit
 
 # add the new backup file and any new file updates (plugins, uploads, etc.) and push them to github
 rm -f .gitignore # remove nodebb's .gitignore file because it will make git ignore our files!
