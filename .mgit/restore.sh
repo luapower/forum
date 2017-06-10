3d431cbc33d7f780c72d600ebc87e7d9ce5e1f5a
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2017-06-10 18:50:45 +0100

    unimportant

--- .mgit/restore.sh
+++ .mgit/restore.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 # decrypt/decompress redis backup
 openssl enc -aes-256-cbc -d -in .mgit/dump.rdb.gz -k "$(cat .mgit/secret)" | gunzip - > .mgit/dump.rdb
+
+# replace db
+sudo mv -f .mgit/dump.rdb /var/lib/redis/dump.rdb
